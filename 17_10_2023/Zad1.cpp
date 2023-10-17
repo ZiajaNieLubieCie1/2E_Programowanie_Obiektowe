b68ea5d946cd458e4e1b67a10148a37fa989daac
ZiajaNieLubieCie1 <karollakomiec28@gmail.com>
2023-10-17 14:55:13 +0200

    Zadanie 1 i zadanie 2

---
+++ 17_10_2023/Zad1.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	int i = 0;
+	string haslo = "haslo1";
+	string login = "login1";
+	do 
+	{
+		cout << "Podaj login: " << endl;
+		cin >> login;
+		cout << "Podaj haslo: " << endl;
+		cin >> haslo;
+
+		if (login == "login1" && haslo == "haslo1") {
+			cout << "Zostales zalogowany" << endl;
+			break;
+		}
+		else
+		{
+			cout << "Bledny login lub haslo " << endl;
+			i++;
+		}
+
+		if (i == 3) {
+			cout << "Zaduza liczba powtorzen" << endl;
+			break;
+		}
+	} while (i < 3);
+
+	return 0;
+}
