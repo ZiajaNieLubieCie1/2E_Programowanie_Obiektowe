1d09f5a5fe6144b5ad4178ff7121ec1ddf7b9a8a
ZiajaNieLubieCie1 <145669244+ZiajaNieLubieCie1@users.noreply.github.com>
2024-04-23 22:57:17 +0200

    Update GRA.cpp

--- 23.04.2024/GRA.cpp
+++ 23.04.2024/GRA.cpp
@@ -46,12 +46,44 @@ int main()
 {
 	string sekretneSlowo, pomieszaneSlowo, slowoUzytkownika, kolejnaPartia;
 	int punkty = 0;
-		srand(time(0));
+	srand(time(0));
 
-		while (true)
+	while (true)
+	{
+		string wybor;
+		cout << "Chcesz wpisac nowe slowa do programu? \nTAK \nNIE" << endl; cin >> wybor;
+		if (wybor == "TAK")
+		{
+			string const nazwaPliku("plik3.txt");
+			ofstream mojStrumien("plik3.txt", ios::trunc);
+			
+			if (mojStrumien)
+			{
+				while (true)
+				{
+					string noweslowo, wybor2;
+					cout << "Podaj nowe slowo: " << endl;
+					cin >> noweslowo;
+					mojStrumien << noweslowo << endl;
+					cout << "Chcesz podac jeszcze jakies slowa? \nTAK \nNIE" << endl;
+					cin >> wybor2;
+					if (wybor2 == "NIE")
+					{
+						wybor = "NIE";
+						system("cls");
+						break;
+					}
+				}
+			}
+			else
+			{
+				cout << "BLAD. program nie programuje" << endl;
+			}
+		}
+		else if (wybor == "NIE")
 		{
 			int proby = 5;
-			
+
 			int upunkty = 0;
 			sekretneSlowo = LosojSlowoPlik("plik3.txt");
 			do
@@ -73,19 +105,20 @@ int main()
 					cout << "Zostalo ci: " << proby << " prob" << endl;
 					if (proby == 0)
 					{
-						cout << "Nie masz ju¿ wiecej prob" << endl;
+						cout << "Nie masz juÅ¼ wiecej prob" << endl;
 						break;
 					}
 				}
 			} while (slowoUzytkownika != sekretneSlowo);
 			cout << "Czy chcesz zakonczyc gre? \n TAK/NIE" << endl; cin >> kolejnaPartia;
-				if (kolejnaPartia == "NIE")
-				{
-					break;
-				}
+			if (kolejnaPartia == "NIE")
+			{
+				break;
+			}
 		}
+	}
 
-		cout << "Zdobyte punkty: " << punkty << endl;
-		return 0;
+	cout << "Zdobyte punkty: " << punkty << endl;
+	return 0;
 }
-	
+
