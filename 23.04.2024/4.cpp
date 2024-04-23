8fdee9204566c3eef9743873bbd025fd616ba2e1
ZiajaNieLubieCie1 <145669244+ZiajaNieLubieCie1@users.noreply.github.com>
2024-04-23 15:10:07 +0200

    Update 4.cpp

--- 23.04.2024/4.cpp
+++ 23.04.2024/4.cpp
@@ -6,7 +6,7 @@ using namespace std;
 
 int main()
 {
-	ifstream mojStrumien("C:/Users/Uczen/source/repos/ConsoleApplication4/ConsoleApplication4/plik3.txt");
+	ifstream mojStrumien("plik3.txt");
 	mojStrumien.seekg(0, ios::end);
 
 	int dlugosc;
